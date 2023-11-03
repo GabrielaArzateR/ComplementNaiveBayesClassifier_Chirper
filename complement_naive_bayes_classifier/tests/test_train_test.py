efd4eeeaa09ae31eee79900863268357c6458cc0
GabrielaArzateR <lesliegabrielardz@outlook.com>
2023-11-03 10:51:29 +0100

    test_fu nction modification

--- complement_naive_bayes_classifier/tests/test_train_test.py
+++ complement_naive_bayes_classifier/tests/test_train_test.py
@@ -1,10 +1,11 @@
 
-
 from complement_naive_bayes_classifier.script import train_test
 
 def test_train_test() -> None:
     """Tests the train_test function"""
-    train_test()
-    assert True
+    data_path = "/Users/gabrielaarzate/Desktop/Public_Project/ComplementNaiveBayesClassifier_ChirperL/data/chirper.csv"  # Replace with the path to your test data
+    train_test(data_path)  # Call the train_test function with the test data
 
+if __name__ == '__main__':
+    test_train_test()
 
