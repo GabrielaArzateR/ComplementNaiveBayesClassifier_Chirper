a076de419951b0db12131746dd57c2f80e2d51c2
GabrielaArzateR <lesliegabrielardz@outlook.com>
2023-11-03 10:41:28 +0100

    Adding tests function : test_train_test

---
+++ complement_naive_bayes_classifier/tests/test_train_test.py
@@ -0,0 +1,10 @@
+
+
+from complement_naive_bayes_classifier.script import train_test
+
+def test_train_test() -> None:
+    """Tests the train_test function"""
+    train_test()
+    assert True
+
+
